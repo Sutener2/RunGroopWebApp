8068e3f00332f6eff95ef3ad38b969e9b5247297
Sutener2 <120112438+Sutener2@users.noreply.github.com>
2024-05-02 00:44:15 +0200

    Style for Race page and Club page, image for cart

---
+++ RunGroopWebApp/Migrations/20240430234259_UpdateAddressAddStreet.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RunGroopWebApp.Migrations
+{
+    public partial class UpdateAddressAddStreet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Sreet",
+                table: "Addresses",
+                newName: "Street");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Street",
+                table: "Addresses",
+                newName: "Sreet");
+        }
+    }
+}
